467e207a19392952e19c4604fc14a67537bb9d31
Moschini Richard <moschini@amiu.genova.it>
2026-02-10 10:20:15 +0100

    fix env

--- main.py
+++ main.py
@@ -5,16 +5,15 @@ from idea_api import router as idea_router
 from auth_api import router as auth_router
 from tellus_api import router as tellus_router
 from localizzazione_api import router as localizzazione_router
-
 from logging.handlers import TimedRotatingFileHandler
-
-log_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-
 # Usa la data odierna per il nome del file log
 from datetime import datetime
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+log_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 today_str = datetime.now().strftime("%Y%m%d")
@@ -34,7 +33,7 @@ logging.getLogger("watchfiles.main").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 # Inizializza l'app FastAPI
-app = FastAPI(title="API AMIU SIT", version="1.0.0", description="API per l'accesso ai dati geografici di AMIU",root_path="/test")
+app = FastAPI(title="API AMIU SIT", version="1.0.0", description="API per l'accesso ai dati geografici di AMIU",root_path="/"+ os.getenv("ENVIRONMENT_CONTEXT_PATH"))
 
 # Definizione del router per l'autenticazione (accesso libero)
 app.include_router(prefix="/auth", tags=["Servizi di autenticazione"], router=auth_router)
