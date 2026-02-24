e9a66b3b383386f584c57da296b0be8e6d1d068a
Moschini Richard <moschini@amiu.genova.it>
2026-02-24 12:27:14 +0100

    add cors permission test mobile ionic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 
@@ -40,6 +41,25 @@ logger = logging.getLogger(__name__)
 
 # Inizializza l'app FastAPI
 app = FastAPI(title="API AMIU", version="1.0.0", description="API per l'accesso ai dati AMIU",root_path="/"+ os.getenv("ENVIRONMENT_CONTEXT_PATH"))
+
+
+# 1. Definisci gli "origins" (chi può chiamare la tua API)
+origins = [
+    "http://localhost:8100",   # Il default di Ionic in sviluppo
+    "http://localhost",         # Per i test da mobile
+    "capacitor://localhost",    # Necessario per iOS
+]
+
+# 2. Aggiungi il Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,             # Permette le origini specificate
+    # allow_origins=["*"],             # Scorciatoia: permette TUTTO (meno sicuro)
+    allow_credentials=True,
+    allow_methods=["*"],               # Permette GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],               # Permette tutti gli header (es. Authorization)
+)
+
 
 # Definizione del router per l'autenticazione
 app.include_router(prefix="/auth", tags=["Servizi di autenticazione"], router=auth_router)
